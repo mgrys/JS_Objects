00be14c6d94ba82802b75ffb8ff0115337847d30
Marek Grys <marek.grys@gmail.com>
2017-08-18 21:48:34 +0200

    excercise 11.5 problem with $

--- js/scripts.js
+++ js/scripts.js
@@ -41,7 +41,7 @@ function Button(text) {
 Button.prototype = {
   create: function() {
     var self = this;
-    this.$element = $ (`<button>`);
+    this.$element = $('<button>');
     this.$element.text(this.text);
     this.$element.click(function() {
       alert(self.text);
